a03acfba2727112f39df4505afb553ce75f2a51a
Madson Rocha <madsonsd@gmail.com>
2020-06-21 19:18:23 -0300

    Inclusão do projeto aula_jquery_ajax

---
+++ js/main.js
@@ -0,0 +1,34 @@
+function consultaCep(){
+    $(".barra-progresso").show();
+    var cep = document.getElementById("cep").value;
+    var url = "http://viacep.com.br/ws/" + cep + "/json/";
+    console.log(url);
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function(response){
+            console.log(response);
+            $("#logradouro").html(response.logradouro);
+            $("#bairro").html(response.bairro);
+            $("#localidade").html(response.localidade);
+            $("#uf").html(response.uf);
+            $("#titulo_cep").html("CEP " + response.cep);
+            $(".cep").show();
+            $(".barra-progresso").hide();
+
+            /*
+            $("#logradouro").html(response.logradouro);
+            //document.getElementById("logradouro").innerHTML = response.logradouro;
+            document.getElementById("bairro").innerHTML = response.bairro;
+            document.getElementById("localidade").innerHTML = response.localidade;
+            document.getElementById("uf").innerHTML = response.uf;
+            //alert(response.logradouro);
+            */
+        }
+    })
+}
+
+$(function(){
+    $(".cep").hide();
+    $(".barra-progresso").hide();
+});
